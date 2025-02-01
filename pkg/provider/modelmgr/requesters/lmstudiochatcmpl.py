d0eeb2b3047684907b2d0e8eaad88a7895165cc7
Junyan Qin <rockchinq@gmail.com>
2025-02-01 12:01:07 +0800

    feat: add supports for LM Studio

---
+++ pkg/provider/modelmgr/requesters/lmstudiochatcmpl.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+import openai
+
+from . import chatcmpl
+from .. import requester
+from ....core import app
+
+
+@requester.requester_class("lmstudio-chat-completions")
+class LmStudioChatCompletions(chatcmpl.OpenAIChatCompletions):
+    """LMStudio ChatCompletion API 请求器"""
+
+    client: openai.AsyncClient
+
+    requester_cfg: dict
+
+    def __init__(self, ap: app.Application):
+        self.ap = ap
+
+        self.requester_cfg = self.ap.provider_cfg.data['requester']['lmstudio-chat-completions']
