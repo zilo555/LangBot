191f8866ae11a3896271229e47782a7298155eae
Junyan Qin <rockchinq@gmail.com>
2025-02-12 11:25:28 +0800

    chore(bailian): related configuration

---
+++ pkg/provider/modelmgr/requesters/bailianchatcmpl.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+import openai
+
+from . import chatcmpl
+from .. import requester
+from ....core import app
+
+
+@requester.requester_class("bailian-chat-completions")
+class BailianChatCompletions(chatcmpl.OpenAIChatCompletions):
+    """阿里云百炼大模型平台 ChatCompletion API 请求器"""
+
+    client: openai.AsyncClient
+
+    requester_cfg: dict
+
+    def __init__(self, ap: app.Application):
+        self.ap = ap
+
+        self.requester_cfg = self.ap.provider_cfg.data['requester']['bailian-chat-completions']
