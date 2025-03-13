a74111612e7b7ac27a760319d6667c548aa33c9a
Junyan Qin <rockchinq@gmail.com>
2025-03-13 17:29:05 +0800

    chore: config for wxoa mode

---
+++ pkg/core/migrations/m035_wxoa_mode.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+from .. import migration
+
+
+@migration.migration_class("wxoa-mode", 35)
+class WxoaModeMigration(migration.Migration):
+    """迁移"""
+
+    async def need_migrate(self) -> bool:
+        """判断当前环境是否需要运行此迁移"""
+
+        for adapter in self.ap.platform_cfg.data['platform-adapters']:
+            if adapter['adapter'] == 'officialaccount':
+                if 'Mode' not in adapter:
+                    return True
+        return False
+
+    async def run(self):
+        """执行迁移"""
+        for adapter in self.ap.platform_cfg.data['platform-adapters']:
+            if adapter['adapter'] == 'officialaccount':
+                if 'Mode' not in adapter:
+                    adapter['Mode'] = 'drop'
+
+        await self.ap.platform_cfg.dump_config()
