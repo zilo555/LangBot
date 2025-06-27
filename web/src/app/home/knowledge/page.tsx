22ef1a399e07d676625a82b728e1661f245bc210
Junyan Qin <rockchinq@gmail.com>
2025-06-27 22:18:48 +0800

    feat: add knowledge base page

--- web/src/app/home/knowledge/page.tsx
+++ web/src/app/home/knowledge/page.tsx
@@ -1,5 +1,22 @@
 'use client';
 
+import CreateCardComponent from '@/app/infra/basic-component/create-card-component/CreateCardComponent';
+import styles from './knowledgeBase.module.css';
+
 export default function KnowledgePage() {
-  return <div>KnowledgePage</div>;
+  return (
+    <div>
+      <div className={styles.knowledgeListContainer}>
+        <CreateCardComponent
+          width={'100%'}
+          height={'10rem'}
+          plusSize={'90px'}
+          onClick={() => {
+            // setIsEditForm(false);
+            // setModalOpen(true);
+          }}
+        />
+      </div>
+    </div>
+  );
 }
