9f22b8b585095c893f379b2adf2d6cee2e89e62d
Dong_master <2213070223@qq.com>
2025-08-12 11:21:08 +0800

    fix: be adapter.py func reply_message_chunk agr message_id alter bot_message,and in pipelinemgr.py respback.py agr alter

--- pkg/pipeline/pipelinemgr.py
+++ pkg/pipeline/pipelinemgr.py
@@ -94,10 +94,9 @@ class RuntimePipeline:
             ):
                 result.user_notice.insert(0, platform_message.At(query.message_event.sender.id))
             if await query.adapter.is_stream_output_supported():
-                print(query.resp_messages[-1])
                 await query.adapter.reply_message_chunk(
                     message_source=query.message_event,
-                    message_id=str(query.resp_messages[-1].resp_message_id),
+                    bot_message=query.resp_messages[-1],
                     message=result.user_notice,
                     quote_origin=query.pipeline_config['output']['misc']['quote-origin'],
                     is_final=[msg.is_final for msg in query.resp_messages][0]
