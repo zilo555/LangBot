aa7c08ee002bfe0fd0f90c3eb95e9388b368f39d
Junyan Qin <rockchinq@gmail.com>
2025-08-21 10:15:05 +0800

    chore: release v4.2.1

--- pkg/utils/constants.py
+++ pkg/utils/constants.py
@@ -1,4 +1,4 @@
-semantic_version = 'v4.2.0'
+semantic_version = 'v4.2.1'
 
 required_database_version = 5
 """Tag the version of the database schema, used to check if the database needs to be migrated"""
