4df372052da6dc98321f97689891b0feae9c0532
Junyan Qin <rockchinq@gmail.com>
2025-10-10 22:50:57 +0800

    chore: bump version 4.3.8

--- pkg/utils/constants.py
+++ pkg/utils/constants.py
@@ -1,4 +1,4 @@
-semantic_version = 'v4.3.7'
+semantic_version = 'v4.3.8'
 
 required_database_version = 8
 """Tag the version of the database schema, used to check if the database needs to be migrated"""
